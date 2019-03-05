4929b27dba468b818eaa3676f91faa3712105cfd
Mikhail Rogov <creynir@gmail.com>
2019-03-05 21:35:24 +0200

    Add redux to clicker app

--- src/components/Click.js
+++ src/components/Click.js
@@ -1,50 +1,74 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { createStore } from "redux";
 
 class Click extends Component {
 
-    constructor(props) {
+    constructor() {
         super();
-
-        this.state = {
-            count: props.count
-        };
 
         this.incrementCount = this.incrementCount.bind(this);
         this.decrementCount = this.decrementCount.bind(this);
         this.resetCount = this.resetCount.bind(this);
+        this.updateStore = this.updateStore.bind(this);
+
+        const counter = (state = 0, action) => {
+            switch (action.type) {
+                case "INCREMENT":
+                    return state + 1;
+                case "DECREMENT":
+                    return state - 1;
+                case "RESET":
+                    return 0;
+                default:
+                    return state;
+            }
+        };
+
+        this.store = createStore(counter);
+        this.state = {
+            count: this.store.getState()
+        };
+        this.store.subscribe(this.updateStore);
+    }
+
+    updateStore() {
+        this.setState({count: this.store.getState()});
     }
 
     incrementCount() {
-        this.setState(prevState => ({count: prevState.count + 1}));
+        this.store.dispatch({ type: "INCREMENT"});
     }
 
     decrementCount() {
-        this.setState(prevState => ({count: prevState.count - 1}));
+        this.store.dispatch({ type: "DECREMENT"});
     }
 
     resetCount() {
-        this.setState({count: 0})
+        this.store.dispatch({ type: "RESET"});
     }
 
     render() {
         return (
-            <div>
-                <div>{this.state.count}</div>
-                <button onClick={this.incrementCount}>Increment</button>
-                <button onClick={this.decrementCount}>Decrement</button>
-                <button onClick={this.resetCount}>Reset</button>
+            <div className="container">
+                <div className="clicker border border-secondary rounded">
+                    <div className="clicker-display d-flex align-items-center bg-light text-secondary">
+                        <div className="mx-auto display-1">{this.state.count}</div>
+                    </div>
+                    <div className="clicker-button-panel d-flex flex-row">
+                        <button className="btn btn-success w-100" onClick={ () => this.store.dispatch({ type: "INCREMENT"})}>
+                            <i className="fa fa-plus fa-2x"></i>
+                        </button>
+                        <button className="btn btn-warning w-100" onClick={this.resetCount}>
+                            <i className="fa fa-refresh fa-2x"></i>
+                        </button>
+                        <button className="btn btn-danger w-100" onClick={this.decrementCount}>
+                            <i className="fa fa-minus fa-2x"></i>
+                        </button>
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }
-
-Click.defaultProps = {
-    count: 0
-};
-
-Click.propTypes = {
-    count: PropTypes.number
-};
 
 export default Click;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,71 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
+import './styles/app.scss';
+
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+// const counter = (state = 0, action) => {
+//     switch (action.type) {
+//         case "INCREMENT":
+//             return state + 1;
+//         case "DECREMENT":
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// };
+//
+// console.log(counter(1,{type: "INCREMENT"}));
+// console.log(counter(1,{type: "DECREMENT"}));
+// console.log(counter(0, {type: "ABRAKADABRA"}));
+//
+//
+// const createStore = (reducer) => {
+//     let state;
+//     let listeners = [];
+//
+//     const getState = () => state;
+//
+//     const dispatch = (action) => {
+//         state = reducer(state, action);
+//         listeners.forEach(listener => listener());
+//     };
+//
+//     const subscribe = (listener) => {
+//         listeners.push(listener);
+//     };
+//
+//     dispatch({});
+//
+//     return { getState, dispatch, subscribe };
+// };
+//
+// const store = createStore(counter);
+//
+// console.log(store.getState());
+//
+// store.subscribe(() => {
+//     document.body.innerText = store.getState();
+// });
+//
+// document.addEventListener('click', () => {
+//     store.dispatch({ type: "INCREMENT"});
+// });
+//
+// const observable = [];
+//
+// function subscribe (observer) {
+//     observable.push(observer);
+// }
+//
+// function notify () {
+//     const text = "from observer";
+//     observable.forEach(obs => obs(text));
+// }
+//
+// subscribe(console.log);
+//
+// notify();
+//
